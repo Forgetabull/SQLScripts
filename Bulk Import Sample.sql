daa5aeed69d80ee3b4994999308e3f696ab006ce
Alan Carter <likerawsushi@gmail.com>
2023-03-23 21:17:44 +1000

    Bulk Import Script sample 2

---
+++ Bulk Import Sample.sql
@@ -0,0 +1,10 @@
+BULK INSERT dbo.SouthVelocity FROM 'C:\data\SouthVelocity7.csv' WITH ( FORMAT='CSV', FIRSTROW=2 )
+GO
+/*
+BULK INSERT dbo.SouthVelocity FROM 'C:\data\SouthVelocity6.csv' WITH ( FORMAT='CSV', FIRSTROW=2 )
+GO
+BULK INSERT dbo.SouthVelocity FROM 'C:\data\SouthVelocity5.csv' WITH ( FORMAT='CSV', FIRSTROW=2 )
+GO
+BULK INSERT dbo.SouthVelocity FROM 'C:\data\SouthVelocity4.csv' WITH ( FORMAT='CSV', FIRSTROW=2 )
+GO
+*/
